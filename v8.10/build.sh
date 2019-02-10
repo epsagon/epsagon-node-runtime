61ac2317605cb2a1148f4c8a91bd7a52b5c8c2c6
semantic-release (via TravisCI) <semantic-release@travis>
2019-02-10 10:00:22 +0200

    fix(layer): Adding aws-sdk

--- v8.10/build.sh
+++ v8.10/build.sh
@@ -2,5 +2,5 @@
 
 export NODE_VERSION=8.10.0
 
-docker build --build-arg NODE_VERSION -t node-provided-lambda-v8.10 .
+docker build --no-cache --build-arg NODE_VERSION -t node-provided-lambda-v8.10 .
 docker run --rm node-provided-lambda-v8.10 cat /tmp/node-v${NODE_VERSION}.zip > ./layer.zip
